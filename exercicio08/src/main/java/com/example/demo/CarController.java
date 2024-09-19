072f9246409799fa6c79d8f9d15e0875924adf0d
Jonatas Fernandes Grassi <jonatasnb713@hotmail.com>
2024-09-18 23:49:06 -0300

    ajustes

--- exercicio08/src/main/java/com/example/demo/CarController.java
+++ exercicio08/src/main/java/com/example/demo/CarController.java
@@ -10,20 +10,16 @@ public class CarController {
 
     @PostMapping("/fipe")
     public ResponseEntity<CarResponse> getCarFipeValue(@RequestBody CarRequest carRequest) {
-        // URL da API da Tabela FIPE (ajuste conforme necessário)
         String fipeApiUrl = "https://api.fipe.org.br/{marca}/{modelo}/{ano}";
 
-        // Substituindo os parâmetros da URL
         String url = fipeApiUrl
                 .replace("{marca}", carRequest.getMarca())
                 .replace("{modelo}", carRequest.getModelo())
                 .replace("{ano}", String.valueOf(carRequest.getAno()));
 
-        // Consumindo a API com RestTemplate
         RestTemplate restTemplate = new RestTemplate();
         ResponseEntity<FipeResponse> response = restTemplate.getForEntity(url, FipeResponse.class);
 
-        // Retornando a resposta formatada
         CarResponse carResponse = new CarResponse(response.getBody().getValor(), "Setembro de 2024");
         return ResponseEntity.ok(carResponse);
     }
